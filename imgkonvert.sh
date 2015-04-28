4d112e8c3d3cab65ed4328aded41d3b287f52f73
Aitor Quinza <aitorquinzagarcia@gmail.com>
2015-04-28 07:37:03 +0200

    Mejoras 2

--- imgkonvert.sh
+++ imgkonvert.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 convert=/usr/bin/convert
-# Script to convert all mage files from a directory (parameter) into png format file
+# Script to convert all image files from a directory (parameter) into png format file
 # it also reduces size
-# @author Aitor Quinza Garcia
+
 # Sanity checks ?
-# ...
+nofinalslash=${1%/}
+if [ $# -ne 2]
+then
+	echo "Use correctly: ./imgkonver.sh /picturepath 1|2|3"
+	exit 1
+fi
 
 # Main code
 
@@ -12,13 +17,32 @@ ls -1 $1/*.jpg > imagefiles
 cat imagefiles
 while read imgfile 
 do
-    echo "IMG file: $imgfile"
-    lengthname=${#imgfile}
-    echo "Length: $lengthname"
-    namefile=${imgfile:0:$lengthname-4}
-    echo "namefile: $namefile"
-    convert $imgfile -resize 200x200 $namefile.png    
-
+case $2 in
+	1)
+	echo "IMG file: $imgfile"
+	lengthname=${#imgfile}
+	echo "Length: $lengthname"
+	namefile=${imgfile:0:$lengthname-4}
+	echo "namefile: $namefile"
+	convert $imgfile -auto-gamma -resize 200x200 $namefile.png;;
+	2)
+	echo "IMG file: $imgfile"
+	lengthname=${#imgfile}
+	echo "Length: $lengthname"
+	namefile=${imgfile:0:$lengthname-4}
+	echo "namefile: $namefile"
+	convert $imgfile -auto-gamma -resize 300x300 $namefile.png;;
+	3)
+	echo "IMG file: $imgfile"
+	lengthname=${#imgfile}
+	echo "Length: $lengthname"
+	namefile=${imgfile:0:$lengthname-4}
+	echo "namefile: $namefile"
+	convert $imgfile -auto-gamma -resize 400x400 $namefile.png;;
+	*)
+	echo "Put a valid option";;
+	
+esac
 done < imagefiles
 rm imagefiles
 exit 0
